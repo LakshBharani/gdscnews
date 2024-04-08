6f75c30b304023a4dc6daa24c662821af5a393c2
LakshBharani <laksh.bharani@gmail.com>
2024-04-08 18:06:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gdscnews
+
+A Hacker News App that provides a seamless way to see latest news.
---
+++ android/app/src/main/kotlin/com/example/gdscnews/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gdscnews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
